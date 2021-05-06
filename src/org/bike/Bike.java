473de8e8e0ad14ba1cb4f3f48d399b350d372f9e
mr-epic16 <srm143muruga@gmail.com>
2021-05-06 19:44:18 +0530

    demo project

--- src/org/bike/Bike.java
+++ src/org/bike/Bike.java
@@ -2,6 +2,8 @@ package org.bike;
 
 public interface Bike {
 	abstract void cost(int a);
-	abstract void speed(int b,String name);
 
+	abstract void speed(int b, String name);
+
+	abstract void kmph();
 }
--- src/org/bike/Ktm.java
+++ src/org/bike/Ktm.java
@@ -4,30 +4,35 @@ public class Ktm implements Bike {
 
 	@Override
 	public void cost(int a) {
-		System.out.println("Price is "+a);
-		
+		System.out.println("Price is " + a);
+
 	}
 
 	@Override
-	public void speed(int b,String name) {
-		
-		System.out.println("Bike speed "+(b+name));
-	}
-	
-		private void cost(float c) {
-			System.out.println("ktm Bike price is "+(c));
+	public void speed(int b, String name) {
 
-		}
-		private void speed(String name1) {
-		
-System.out.println("Bike Kph "+name1);
-		}
-	
-public static void main(String[] args) {
-	Ktm c = new Ktm();
-	c.cost(300000f);
-	c.speed("200");
-	c.cost(200000);
-	c.speed(125,"kph");
-}
+		System.out.println("Bike speed " + (b + name));
+	}
+
+	private void cost(float c) {
+		System.out.println("ktm Bike price is " + (c));
+
+	}
+
+	private void speed(String name1) {
+
+		System.out.println("Bike Kph " + name1);
+	}
+
+	public static void main(String[] args) {
+		Ktm c = new Ktm();
+		c.cost(300000f);
+		c.speed("200");
+		c.cost(200000);
+		c.speed(125, "kph");
+	}
+
+	public void kmph() {
+		System.out.println("KMPH = 150cc");
+	}
 }
